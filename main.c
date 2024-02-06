ecf301788770bd65be4ba862c0c94539640c0a08
jessespringborn <157310555+jessespringborn@users.noreply.github.com>
2024-02-05 23:22:18 -0600

    Added submodules

--- main.c
+++ main.c
@@ -1,8 +1,45 @@
 #include <stdio.h>
 
+#include <vulkan/vulkan.h>
+
+#include <vk_mem_alloc.h>
+
+#include <SDL.h>
+
+#define CIMGUI_DEFINE_ENUMS_AND_STRUCTS
+#include <cimgui.h>
+
 int
 main()
 {
+    if (SDL_Init(SDL_INIT_VIDEO) != 0)
+    {
+        printf("SDL_Init Error: %s\n", SDL_GetError());
+        return 1;
+    }
+
+    SDL_Window* window = SDL_CreateWindow("Vulkan", 800, 600,
+                                          SDL_WINDOW_VULKAN);
+    if (window == NULL)
+    {
+        printf("SDL_CreateWindow Error: %s\n", SDL_GetError());
+        return 1;
+    }
+
+    // game loop
+    bool running = true;
+    while (running)
+    {
+        SDL_Event event;
+        while (SDL_PollEvent(&event))
+        {
+            if (event.type == SDL_EVENT_QUIT)
+            {
+                running = false;
+            }
+        }
+    }
+
     printf("Hello, World!\n");
     return 0;
 }
